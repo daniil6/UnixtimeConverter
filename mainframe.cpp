c83150778a916d60ecdb8a5504d055ab5528bd64
Daniil <rbkwork@yandex.ru>
2019-12-26 13:41:41 +0300

    Add timer and show value secund if data_time and show data_time

--- mainframe.cpp
+++ mainframe.cpp
@@ -3,20 +3,37 @@
 
 // 00:00 1 января 1970
 
+BEGIN_EVENT_TABLE(CMainFrame, wxFrame)
+EVT_TIMER(TIMER_ID, CMainFrame::OnTimerProcess)
+END_EVENT_TABLE()
+
 CMainFrame::CMainFrame(wxFrame* frame)
     : wxFrame(frame, wxID_ANY, wxT("UnixtimeConverter"))
+    , m_timer(this, TIMER_ID)
 {
     wxBoxSizer* main_sizer = new wxBoxSizer(wxHORIZONTAL);
-    wxTextCtrl* m_dataTime = new wxTextCtrl(this, NewControlId(), wxT(""));
-    main_sizer->Add(m_dataTime);
+    m_secDataTime = new wxTextCtrl(this, NewControlId(), wxT(""));
+    m_dataTime = new wxTextCtrl(this, NewControlId(), wxT("0000-00-00 00:00:00"), wxDefaultPosition, wxSize(130, 10));
+    main_sizer->Add(m_secDataTime, 1, wxEXPAND);
+    main_sizer->Add(m_dataTime, 1, wxEXPAND);
     this->SetSizerAndFit(main_sizer);
 
-    long res = CalculateTime(36, 12, 45, 4, 5, 2020);
-
-    wxString temp = wxString::Format(wxT("%i"), res);
-    m_dataTime->SetValue(temp);
+    m_timer.Start(1000);
 }
 
 CMainFrame::~CMainFrame()
 {
+}
+
+void CMainFrame::OnTimerProcess(wxTimerEvent& event)
+{
+    wxDateTime dt = wxDateTime::Now();
+
+    tDataTime dataTime{ (uint8_t)dt.GetSecond(), (uint8_t)dt.GetMinute(), (uint8_t)(dt.GetHour() + 3),
+        (uint8_t)dt.GetDay(), (uint8_t)dt.GetMonth(), (uint16_t)dt.GetYear() };
+    long res = CalculateTime(dataTime);
+    wxString t_string = wxString::Format(wxT("%i"), res);
+
+    m_secDataTime->SetValue(t_string);
+    m_dataTime->SetValue(dt.Format(wxT("%Y-%m-%d %H:%M:%S")));
 }
--- mainframe.h
+++ mainframe.h
@@ -5,6 +5,17 @@
 
 class CMainFrame : public wxFrame
 {
+private:
+    enum { TIMER_ID = wxID_HIGHEST + 1 };
+
+    wxTimer m_timer;
+    wxTextCtrl* m_secDataTime;
+    wxTextCtrl* m_dataTime;
+
+    void OnTimerProcess(wxTimerEvent& event);
+
+    DECLARE_EVENT_TABLE()
+
 public:
     CMainFrame(wxFrame* frame);
     ~CMainFrame();
