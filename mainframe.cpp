8679c30418093f8264e5788c38c8b4416ce4d599
Daniil <rbkwork@yandex.ru>
2020-01-13 19:37:16 +0300

    Add table for convert data and time to sec

--- mainframe.cpp
+++ mainframe.cpp
@@ -11,18 +11,74 @@ CMainFrame::CMainFrame(wxFrame* frame)
     : wxFrame(frame, wxID_ANY, wxT("UnixtimeConverter"))
     , m_timer(this, TIMER_ID)
 {
-    wxBoxSizer* main_sizer = new wxBoxSizer(wxHORIZONTAL);
+
     m_secDataTime = new wxTextCtrl(this, NewControlId(), wxT(""));
     m_dataTime = new wxTextCtrl(this, NewControlId(), wxT("0000-00-00 00:00:00"), wxDefaultPosition, wxSize(130, 10));
-    main_sizer->Add(m_secDataTime, 1, wxEXPAND);
-    main_sizer->Add(m_dataTime, 1, wxEXPAND);
-    this->SetSizerAndFit(main_sizer);
+
+    wxButton* m_convert = new wxButton(this, NewControlId(), wxT("Convert"));
+    m_result = new wxTextCtrl(this, NewControlId(), wxT(""));
+
+    m_grid = new wxGrid(this, NewControlId());
+    m_grid->CreateGrid(1, 6);
+    m_grid->SetRowLabelSize(0);
+    m_grid->SetColLabelValue(0, wxT("sec"));
+    m_grid->SetColLabelValue(1, wxT("min"));
+    m_grid->SetColLabelValue(2, wxT("hors"));
+    m_grid->SetColLabelValue(3, wxT("day"));
+    m_grid->SetColLabelValue(4, wxT("mounth"));
+    m_grid->SetColLabelValue(5, wxT("yaer"));
+
+    wxBoxSizer* main_box = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* h_box = nullptr;
+    wxBoxSizer* v_box = nullptr;
+
+    h_box = new wxBoxSizer(wxHORIZONTAL);
+    h_box->Add(m_secDataTime, 1, wxEXPAND);
+    h_box->Add(m_dataTime, 1, wxEXPAND);
+    main_box->Add(h_box);
+
+    v_box = new wxBoxSizer(wxVERTICAL);
+    v_box->Add(m_convert);
+    v_box->Add(m_result);
+
+    h_box = new wxBoxSizer(wxHORIZONTAL);
+    h_box->Add(m_grid);
+    h_box->Add(v_box);
+
+    main_box->Add(h_box);
+
+    this->SetSizerAndFit(main_box);
+
+    Bind(wxEVT_BUTTON, &CMainFrame::OnConvert, this, m_convert->GetId());
 
     m_timer.Start(1000);
 }
 
 CMainFrame::~CMainFrame()
 {
+}
+
+void CMainFrame::OnConvert(wxCommandEvent& event)
+{
+    uint8_t countCol = m_grid->GetNumberCols();
+    wxString t_str[countCol];
+    int t_int[countCol];
+    for(int i = 0; i < countCol; i++) {
+        t_str[i] = m_grid->GetCellValue(0, i);
+        if(t_str[i].empty() == true)
+            t_str[i] = wxT("1");
+    }
+
+    for(int i = 0; i < countCol; i++)
+        t_int[i] = wxAtoi(t_str[i]);
+
+    tDataTime dataTime{ (uint8_t)t_int[0], (uint8_t)t_int[1], (uint8_t)t_int[2], (uint8_t)t_int[3], (uint8_t)t_int[4],
+        (uint16_t)t_int[5] };
+
+    long res = CalculateTime(dataTime);
+    t_str[0] = wxString::Format(wxT("%i"), res);
+
+    m_result->SetValue(t_str[0]);
 }
 
 void CMainFrame::OnTimerProcess(wxTimerEvent& event)
--- mainframe.h
+++ mainframe.h
@@ -1,7 +1,8 @@
 #ifndef CMAINFRAME_H
 #define CMAINFRAME_H
 
-#include "wx/wx.h"
+#include <wx/grid.h>
+#include <wx/wx.h>
 
 class CMainFrame : public wxFrame
 {
@@ -11,8 +12,11 @@ private:
     wxTimer m_timer;
     wxTextCtrl* m_secDataTime;
     wxTextCtrl* m_dataTime;
+    wxGrid* m_grid;
+    wxTextCtrl* m_result;
 
     void OnTimerProcess(wxTimerEvent& event);
+    void OnConvert(wxCommandEvent& event);
 
     DECLARE_EVENT_TABLE()
 
